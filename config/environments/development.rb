b89347df41403447ed0e4f9b97c1c339c1287efd
Logan Riffell <lkriffell@gmail.com>
2022-05-15 13:06:02 -0400

    Allow hosting on replit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,4 +49,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow hosting on *.repl.co hosts
+  config.hosts << /.*\.repl.co/
 end
