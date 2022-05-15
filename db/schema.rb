1bb209ebe0a8ce7ac2fe4792167640f7fda125e0
Logan Riffell <lkriffell@gmail.com>
2022-05-15 11:25:48 -0400

    Update schema and add FK migration for Item

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_14_195307) do
+ActiveRecord::Schema.define(version: 2022_05_15_151959) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.float "weight_lb"
     t.integer "count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "warehouse_id"
+  end
+
+  create_table "warehouses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
