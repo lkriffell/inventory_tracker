c4197306634a0dcaf9be656324e456e6ababcd64
Logan Riffell <lkriffell@gmail.com>
2022-05-15 11:52:04 -0400

    Add model methods to handle a warehouse's capacity

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,6 +1,24 @@
 class Warehouse < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :location
+  validates_presence_of :capacity
 
   has_many :items
+
+  def capacity_left()
+    capacity_taken = 0
+    items.each do |item|
+      capacity_taken += item.count 
+    end
+    capacity - capacity_taken
+  end
+
+  def can_add_item_to_warehouse?(item)
+    capacity_left = capacity_left()
+    if item.count <= capacity_left
+      true
+    else
+      false
+    end
+  end
 end
