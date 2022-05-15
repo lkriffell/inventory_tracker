83efcc6646abb8d003ef3456d8150c64bc05e782
Logan Riffell <lkriffell@gmail.com>
2022-05-15 19:37:20 -0400

    Convert from resources to more explicit routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items, :only => [:index, :create, :edit]
+  get "/items", to: "items#index"
+  get "/items/new", to: "items#create"
+  get "/items/:id/edit", to: "items#edit"
 
-  post "/warehouses", to: "warehouses#create"
+
+  get "/warehouses/new", to: "warehouses#create"
   get "/warehouses/:id/add_item/:item_id", to: "warehouses#add_item"
 end
